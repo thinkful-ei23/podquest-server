9daa7b50648a085ca2bf8499e68bd6527ffefce0
Clinton Owen <clint@clintonowen.com>
2018-11-05 09:49:03 -0600

    Add validation for podcast channel info to prevent loading errors

--- routes/podcast.js
+++ routes/podcast.js
@@ -8,46 +8,56 @@ const router = express.Router();
 
 /* ========== XML/RSS HANDLER ========== */
 const getXML = url => {
-	let reqHandler = url.includes('http://') ? http : https;
-	return new Promise((resolve, reject) => {
-		let req = reqHandler.get(url, res => {
-			let output = '';
-			res.setEncoding('utf8');
-			res.on('data', chunk => {
-				output += chunk;
-			});
-			res.on('end', () => {
-				try {
-					parseString(output, (err, res) => {
-						resolve(res);
-					});
-				} catch (err) {
-					reject(err);
-				}
-			});
-		});
-		req.on('error', err => {
-			reject(err);
-		});
-		req.end();
-	});
+  let reqHandler = url.includes('http://') ? http : https;
+  return new Promise((resolve, reject) => {
+    let req = reqHandler.get(url, res => {
+      let output = '';
+      res.setEncoding('utf8');
+      res.on('data', chunk => {
+        output += chunk;
+      });
+      res.on('end', () => {
+        try {
+          parseString(output, (err, res) => {
+            resolve(res);
+          });
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+    req.on('error', err => {
+      reject(err);
+    });
+    req.end();
+  });
 };
 
 /* ========== POST/RETRIEVE A PODCAST CHANNEL ========== */
 router.post('/', (req, res, next) => {
-	const { feedUrl } = req.body;
-
+  const { feedUrl } = req.body;
+  
   getXML(feedUrl)
     .then(data => {
       const channel = data.rss.channel[0];
+      // console.log('channel', channel);
+
+      const title = channel.title ? channel.title[0] : null;
+      const author = channel['itunes:author'] ? channel['itunes:author'][0] : null;
+      const description = channel.description ? channel.description[0] : null;
+      const genres = channel['itunes:category'] ? channel['itunes:category'].map(cat => cat.$.text) : null;
+      const episodes = channel.item ? channel.item : null;
+      const link = channel.link ? channel.link[0] : null;
+      const image = channel['itunes:image'] ? channel['itunes:image'][0].$.href : null;
+
       const channelInfo = {
-        title: channel.title[0],
-        author: channel['itunes:author'][0],
-        description: channel.description[0],
-        genres: channel['itunes:category'].map(cat => cat.$.text),
-        episodes: channel.item,
-        link: channel.link[0],
-        image: channel['itunes:image'][0].$.href,
+        title,
+        author,
+        description,
+        genres,
+        episodes,
+        link,
+        image,
         feedUrl
       };
       res.json(channelInfo);
