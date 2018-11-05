b4ac509bf73fd5d5014ecfe2b438eef71ebdcb72
Chris Gantz <chrisgantz836@gmail.com>
2018-11-05 12:48:51 -0700

    added guid as id to stop dupl

--- routes/favorite.js
+++ routes/favorite.js
@@ -20,7 +20,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const { feedUrl, title, mediaUrl } = req.body;
+  const { feedUrl, title, mediaUrl, guid } = req.body;
   const userId = req.user.id;
   if(!feedUrl) {
     const err = new Error('Missing feedUrl in req body');
@@ -37,8 +37,13 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
+  if(!guid) {
+    const err = new Error('Missing guid in req body');
+    err.status = 400;
+    return next(err);
+  }
 
-  const saveFav = { feedUrl, title, mediaUrl, userId };
+  const saveFav = { feedUrl, title, mediaUrl, guid, userId };
 
   Favorite.create(saveFav)
     .then(results =>
