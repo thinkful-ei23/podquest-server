868df5c168b873e93ef7a950031977c079ef1ec9
Clinton Owen <clint@clintonowen.com>
2018-11-13 11:13:30 -0600

    Deal with ECONNREFUSED RSS errors

--- routes/podcast.js
+++ routes/podcast.js
@@ -40,7 +40,7 @@ router.post('/', (req, res, next) => {
   getXML(feedUrl)
     .then(data => {
       // console.log('data', JSON.stringify(data));
-      if (!data.rss) {
+      if (!data || !data.rss) {
         return res.status(404).json({
           code: 404,
           reason: 'RSSError',
@@ -72,6 +72,14 @@ router.post('/', (req, res, next) => {
       res.json(channelInfo);
     })
     .catch(err => {
+      if (err.code === 'ECONNREFUSED') {
+        return res.status(404).json({
+          code: 404,
+          reason: 'RSSError',
+          message: `RSS Feed not found for ${feedUrl}`,
+          location: feedUrl
+        });
+      }
       next(err);
     });
 });
